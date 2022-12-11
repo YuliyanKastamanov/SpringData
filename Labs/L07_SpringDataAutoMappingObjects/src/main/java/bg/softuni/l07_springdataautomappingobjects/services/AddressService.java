699e11e66aaa323327270762e3fa3a8541429e9e
Yuliyan Kastamanov <92514049+YuliyanKastamanov@users.noreply.github.com>
2022-12-11 14:58:15 +0200

    Delete Labs/L07_SpringDataAutoMappingObjects directory

--- Labs/L07_SpringDataAutoMappingObjects/src/main/java/bg/softuni/l07_springdataautomappingobjects/services/AddressService.java
+++
@@ -1,8 +0,0 @@
-package bg.softuni.l07_springdataautomappingobjects.services;
-
-import bg.softuni.l07_springdataautomappingobjects.entities.Address;
-import bg.softuni.l07_springdataautomappingobjects.entities.dtos.AddressDTO;
-
-public interface AddressService {
-    Address create(AddressDTO data);
-}
